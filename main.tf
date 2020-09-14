250716ec2f23cffd2c7a044b48f84588274a6152
John Inge Muldal Erlandsen <john@varleg.no>
2020-09-14 14:19:36 +0200

    add backend config

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.27.0"
+  features {}
+}
 resource "azurerm_resource_group" "main" {
   name     = "rg-tf-sandjohn-01"
   location = "West Europe"
