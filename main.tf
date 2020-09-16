fed472181f935c45fb36a2fb97c12aff08b089f2
John Inge Muldal Erlandsen <john@varleg.no>
2020-09-16 16:22:03 +0200

    tfApi

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
   }
 }
 provider "azurerm" {
-  version = "=2.27.0"
+  version         = "=2.27.0"
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
   client_id       = var.client_id
