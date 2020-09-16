eb17f7895b7e685701a8fc5dcd8cf425d567d68b
John Inge Muldal Erlandsen <john@varleg.no>
2020-09-16 13:31:51 +0200

    add backend

---
+++ backend.hcl
@@ -0,0 +1,4 @@
+resource_group_name         = "rg-terraform"
+storage_account_name        = "tfstatesandboxjohn"
+container_name              = "tfstate"
+subscription_id             = "5ffae18c-eeb0-4706-8a03-0a823e6cccb5"
--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
-provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "=2.27.0"
-  features {}
+terraform {
+  required_providers {
+    provider "azurerm" {
+      version = "=2.27.0"
+      features {}
+      }
+  }
 }
 resource "azurerm_resource_group" "main" {
   name     = "rg-tf-sandjohn-01"
