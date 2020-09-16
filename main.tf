fc2bf7fc64319502c2faf037c5d9ad4011b8fc9d
John Inge Muldal Erlandsen <john@varleg.no>
2020-09-16 13:36:48 +0200

    fix provider

--- main.tf
+++ main.tf
@@ -2,12 +2,10 @@ terraform {
   backend "azurerm" {
     key = "terraform.tfstate"
   }
-  required_providers {
-    provider "azurerm" {
-      version = "=2.27.0"
-      features {}
-      }
-  }
+}
+provider "azurerm" {
+  version = "=2.27.0"
+  features {}
 }
 resource "azurerm_resource_group" "main" {
   name     = "rg-tf-sandjohn-01"
